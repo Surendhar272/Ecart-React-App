a98cbd893a9793ca4afb82d7adddbbad1e9e3958
Surendhar R <surendhar272@gmail.com>
2023-09-19 15:55:10 +0530

    Commit my changes to git

---
+++ src/Components/ratingfunc.js
@@ -0,0 +1,12 @@
+// Created a function to show rating of the item and return the icon accordingly
+export function callRating(value) {
+  if (value === 0 || value < 0) {
+    return (
+      <i className="fa-solid fa-star fa-lg" style={{ color: "#2b7cff" }}></i>
+    );
+  } else {
+    return (
+      <i className="fa-regular fa-star fa-lg" style={{ color: "#2b7cff" }}></i>
+    );
+  }
+}
